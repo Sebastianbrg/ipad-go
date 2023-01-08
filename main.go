bf1e7e0e71997c4cf33b732054a2864571ac480e
Sebastian Berge <sebastianbrg@gmail.com>
2023-01-08 10:46:06 +0000

    cities endpoint generated by ChatGPT

--- main.go
+++ main.go
@@ -1,13 +1,31 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
+type City struct {
+	Name    string `json:"name"`
+	Country string `json:"country"`
+}
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "index.html")
+	})
+	http.HandleFunc("/cities", func(w http.ResponseWriter, r *http.Request) {
+		cities := []City{
+			{Name: "Paris", Country: "France"},
+			{Name: "New York", Country: "United States"},
+			{Name: "London", Country: "United Kingdom"},
+			{Name: "Tokyo", Country: "Japan"},
+			{Name: "Sydney", Country: "Australia"},
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(cities)
 	})
 
 	fmt.Println("Starting server on port 8080...")
